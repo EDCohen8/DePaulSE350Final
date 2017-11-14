a1f42eb8e15ac56a73b7047cc6c66842d241e44d
EDCohen8 <ethan.cohen1997@gmail.com>
2017-11-13 21:41:00 -0600

    Added Movement for Pirates

---
+++ EthanCohenFinal/src/PirateMovementLookForTreasure.java
@@ -0,0 +1,147 @@
+import java.util.Observable;
+
+public class PirateMovementLookForTreasure implements PirateMovementInterface {
+
+	int x, y;
+	Grid map = Grid.getInstance();
+	GamePiece[][] gameMap = map.getMap();
+	boolean left = true, down = true, moved;
+
+	@Override
+	public void updateStrat(Observable o, Object arg, PirateShip pir) {
+		// TODO Auto-generated method stub
+		PlayerShip ship = (PlayerShip) o;
+		x = (int)pir.getLocation().getX();
+		y = (int)pir.getLocation().getY();
+		moved = false;
+		if(left){
+			if(x > 0){
+				x--;
+				while(x != 0 && (gameMap[y][x] instanceof IslandPiece || gameMap[y][x] instanceof PirateShip)) {
+					x--;
+				}
+				if(gameMap[y][x] instanceof PlayerShip || gameMap[y][x] instanceof TreasurePiece){
+					System.out.println("Game Over");
+					moved = true;
+				}
+				if(gameMap[y][x] instanceof OceanPiece){
+					gameMap[(int)pir.getLocation().getY()][(int)pir.getLocation().getX()] = new OceanPiece((int)pir.getLocation().getX(), (int)pir.getLocation().getY());
+					gameMap[y][x] = pir;
+					moved = true;
+				}
+			}
+			if(x == 0 && !moved){
+				left = false;
+				if(down){
+					if(++y < gameMap[0].length){
+						while(gameMap[y][x] instanceof IslandPiece || gameMap[y][x] instanceof PirateShip){
+							x= x+1;
+						}
+						if(gameMap[y][x] instanceof PlayerShip  || gameMap[y][x] instanceof TreasurePiece){
+							System.out.println("Game Over");
+							moved = true;
+						}
+						else if(gameMap[y][x] instanceof OceanPiece){
+							gameMap[(int)pir.getLocation().getY()][(int)pir.getLocation().getX()] = new OceanPiece((int)pir.getLocation().getX(), (int)pir.getLocation().getY());
+							gameMap[y][x] = pir;
+							moved = true;
+						}
+					}
+					if(y == gameMap[0].length - 1){
+						down = false;
+					}
+				}
+				else if(!down){
+					if(--y > 0){
+						while(gameMap[y][x] instanceof IslandPiece || gameMap[y][x] instanceof PirateShip && x > 0){
+							x= x-1;
+						}
+						if(gameMap[y][x] instanceof PlayerShip){
+							System.out.println("Game Over");
+							moved = true;
+						}
+						if(gameMap[y][x] instanceof IslandPiece){
+							gameMap[(int)pir.getLocation().getY()][(int)pir.getLocation().getX()] = new OceanPiece((int)pir.getLocation().getX(), (int)pir.getLocation().getY());
+							gameMap[y][x] = pir;
+							moved = true;
+						}
+					}
+					if(y == 0) {
+						down = true;
+					}
+				}
+			}
+		}
+		if(!left && !moved){
+			if(++x < gameMap.length-1) {
+				while(x != gameMap.length-1 && (gameMap[y][x] instanceof IslandPiece || gameMap[y][x] instanceof PirateShip) && x < gameMap.length -1) {
+					x++;
+				}
+				if(gameMap[y][x] instanceof PlayerShip || gameMap[y][x] instanceof TreasurePiece){
+					System.out.println("Game Over");
+					moved = true;
+				}
+				if(gameMap[y][x] instanceof OceanPiece){
+					gameMap[(int)pir.getLocation().getY()][(int)pir.getLocation().getX()] = new OceanPiece((int)pir.getLocation().getX(), (int)pir.getLocation().getY());
+					gameMap[y][x] = pir;
+					moved = true;
+				}
+			}
+
+		}
+		if(x == gameMap.length -1 && !moved){
+			left = true;
+			if(down){
+				if(++y < gameMap[0].length - 1 ){
+					while((gameMap[y][x] instanceof IslandPiece || gameMap[y][x] instanceof PirateShip) && x < gameMap.length-1){
+						x= x-1;
+					}
+					if(gameMap[y][x] instanceof PlayerShip  || gameMap[y][x] instanceof TreasurePiece){
+						System.out.println("Game Over");
+						moved = true;
+					}
+					if(gameMap[y][x] instanceof OceanPiece){
+						gameMap[(int)pir.getLocation().getY()][(int)pir.getLocation().getX()] = new OceanPiece((int)pir.getLocation().getX(), (int)pir.getLocation().getY());
+						gameMap[y][x] = pir;
+						moved = true;
+					}
+				}
+				if(y == gameMap[0].length - 1){
+					down = false;
+				}
+			}
+			else if(!down){
+				if(--y > 0){
+					while(gameMap[y][x] instanceof IslandPiece || gameMap[y][x] instanceof PirateShip){
+						x= x-1;
+					}
+					if(gameMap[y][x] instanceof PlayerShip){
+						System.out.println("Game Over");
+						moved = true;
+					}
+					if(gameMap[y][x] instanceof IslandPiece){
+						gameMap[(int)pir.getLocation().getY()][(int)pir.getLocation().getX()] = new OceanPiece((int)pir.getLocation().getX(), (int)pir.getLocation().getY());
+						gameMap[y][x] = pir;
+						moved = true;
+					}
+				}
+				if(y == 0){
+					down = true;
+				}
+			}
+		}
+	}
+
+	@Override
+	public int getY() {
+		// TODO Auto-generated method stub
+		return y;
+	}
+
+	@Override
+	public int getX() {
+		// TODO Auto-generated method stub
+		return x;
+	}
+
+}
